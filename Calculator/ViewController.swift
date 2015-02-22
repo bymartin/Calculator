bfceb7b8697958901ea49cf4ef891107bbabccf2
Barry Martin <bymartin@verizon.net>
2015-02-21 23:49:50 -0600

    Assn 1: Required Task 3: add sin/cos/pi

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -40,11 +40,14 @@ class ViewController: UIViewController
         }
         
         switch operation {
-        case "×": performOperation { $0 * $1 }
-        case "÷": performOperation { $1 / $0 }
-        case "+": performOperation { $0 + $1 }
-        case "−": performOperation { $1 - $0 }
-        case "√": performOperation { sqrt($0) }
+        case "×":   performOperation { $0 * $1 }
+        case "÷":   performOperation { $1 / $0 }
+        case "+":   performOperation { $0 + $1 }
+        case "−":   performOperation { $1 - $0 }
+        case "√":   performOperation { sqrt($0) }
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "∏":   appendConstant(M_PI)
         default: break
         }
     }
@@ -61,6 +64,14 @@ class ViewController: UIViewController
             displayValue = operation(operandStack.removeLast())
             enter()
         }
+    }
+    
+    func appendConstant(constant: Double) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        display.text = "\(constant)"
+        enter()
     }
     
     func multiply(op1: Double, op2: Double) -> Double {
